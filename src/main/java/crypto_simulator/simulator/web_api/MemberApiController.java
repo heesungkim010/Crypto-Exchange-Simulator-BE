d2d3f9ba8449d59602333c8ea2c84ce0ad7eb05d
heesungkim010 <heesungkim010@gmail.com>
2022-03-18 10:13:20 +0900

    add JWT

--- src/main/java/crypto_simulator/simulator/web_api/MemberApiController.java
+++ src/main/java/crypto_simulator/simulator/web_api/MemberApiController.java
@@ -7,6 +7,7 @@ import crypto_simulator.simulator.service.PositionService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,11 @@ import java.security.NoSuchAlgorithmException;
 public class MemberApiController {
     private final MemberService memberService;
     private final PositionService positionService;
+
+    @GetMapping("/api/members")
+    public CreateMemberResponse hii(@RequestBody CreateMemberRequest request) throws NoSuchAlgorithmException { // request received
+        return new CreateMemberResponse("123",true);
+    }
 
     @PostMapping("/api/members")
     public CreateMemberResponse registerUser(@RequestBody CreateMemberRequest request) throws NoSuchAlgorithmException { // request received

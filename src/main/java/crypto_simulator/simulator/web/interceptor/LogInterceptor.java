f6a23234a1d8748fb9ce61aa51b4eadfb13a89bc
heesungkim010 <heesungkim010@gmail.com>
2022-04-03 19:20:45 +0900

    login(interceptor)

--- src/main/java/crypto_simulator/simulator/web/interceptor/LogInterceptor.java
+++ src/main/java/crypto_simulator/simulator/web/interceptor/LogInterceptor.java
@@ -3,6 +3,7 @@ package crypto_simulator.simulator.web.interceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -15,11 +16,12 @@ public class LogInterceptor implements HandlerInterceptor {
                              Object handler) throws Exception {
 
         String requestURI = request.getRequestURI();
-
         log.info("prehandle...{}", requestURI);
         HttpSession session = request.getSession(false);
-
+        Cookie[] cookies = request.getCookies();
         log.info("session : {}", session);
+        log.info("cookies: {}", cookies);
+
         return true;
     }
 }
---
+++ src/main/java/crypto_simulator/simulator/web/login/SessionConst.java
@@ -0,0 +1,5 @@
+package crypto_simulator.simulator.web.login;
+
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
